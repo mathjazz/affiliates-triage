a43975f5e9b0a9426a4da657510b5a39052174e2
Milos Dinic <milos@mozilla.com>
2012-03-08 18:23:55 +0100

    adding parsing rules for categories, locales and summaries

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ $(function() {
   var locales = {};
 
   function render() {
-  	for (var locale in locales) {
-  	  var ids = [];
-  	  $(locales[locale]).each(function() {
+    for (var locale in locales) {
+      var ids = [];
+      $(locales[locale]).each(function() {
         ids.push(this.id);
-  	  });
+      });
       $('table').append(
         '<tr class="' + locale + '"><td>' + 
         '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=' + ids.join(', ') + '">' + locale + '</a>' + 
@@ -20,46 +20,70 @@ $(function() {
   }
 
   /* Parses locales and categories from bug summary:
-   * [locale code][and][banner][categories]
+   * "[locale code][and][banner][categories] And some summary text left"
    */
+
   function parse(summary) {
-  	var meta = [];
-    $(summary.split("[")).each(function() {
-      var temp = this.split("]");
-      if (temp.length === 2) {
-        meta.push(temp[0]);
+    var meta = {};
+    temp = summary.split(" ");
+    parameters = temp[0];
+    meta.summary = temp.slice(1).join(" ");
+    var params = [];
+    $(parameters.split("[")).each(function() {
+      var tempParams = this.split("]");
+      for (var i = 0; i < tempParams.length; i++) {
+        if (tempParams[i] == "") {        
+          tempParams.splice(i, 1);
+          i--;
+        }
+      }
+      if (tempParams.length >= 1){
+          params.push(tempParams.join(""));
       }
     });
+    meta.locale = params[0];
+    meta.categories = params.slice(1);
+    /* testing
+    console.log("params:" + params);
+    console.log("locale: " + meta.locale + "; categories: " + meta.categories + "; summary: " + meta.summary);
+    */
     return meta;
   }
 
   function add(bug) {
     var meta = parse(bug.summary);
-    if (meta.length === 0) {
-      meta = ["Other bugs"];
+    if (meta.categories.length === 0) {
+      meta = ["other"];
     }
 
-    var o = {
+    var bugObject = {
           id: bug.id,
-          categories: meta
+          categories: meta.categories,
+          summary: meta.summary,
         },
-        locale = meta[0];
+        locale = meta.locale;
 
     if (!locales[locale]) {
-      locales[locale] = [o];
+      locales[locale] = [bugObject.locale];
     } else {
-      locales[locale].push(o);
+      locales[locale].push(bugObject.locale);
     }
   }
   
-  $.ajax({
-  	url: 'https://api-dev.bugzilla.mozilla.org/0.9/bug', 
-  	dataType: 'json',
-  	data: {
+  $.ajax({ 
+
+    /*  Define a URL for API
+        Response contains the bug with all the properties
+    */
+    
+    url: 'https://api-dev.bugzilla.mozilla.org/0.9/bug', 
+    dataType: 'json',
+    data: {
       product: 'Websites',
       component: 'affiliates.mozilla.org banners'
     }, 
-  	success: function(data) {
+    success: function(data) {
+      /* if we get successful fetch call add() for each bug in response */
       $(data.bugs).each(function() {
         add(this);
       });
